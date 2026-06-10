0a9bdc569c64d90ba931c3502f0d747c6cadeabf
Fineias Judas <judasfineias@gmail.com>
2026-06-10 15:50:35 +0100

    fix: corrigir MIDDLEWARE no settings.py once again

--- mockinterview/settings.py
+++ mockinterview/settings.py
@@ -132,6 +132,6 @@ from pathlib import Path
 SECRET_KEY = os.environ.get('SECRET_KEY', 'chave-local-apenas')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
-
+ 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
